93661a12a870b2ed734476cc92e24ee4c65b51ce
Alexander Pipelka <alexander.pipelka@gmail.com>
2013-01-07 14:14:57 +0100

    start streaming when the video stream has been parsed

--- src/live/livestreamer.c
+++ src/live/livestreamer.c
@@ -591,6 +591,10 @@ bool cLiveStreamer::IsReady()
 
   for (std::list<cTSDemuxer*>::iterator i = m_Demuxers.begin(); i != m_Demuxers.end(); i++)
   {
+    if((*i)->IsParsed() && (*i)->GetContent() == cStreamInfo::scVIDEO) {
+      bAllParsed = true;
+      break;
+    }
     if (!(*i)->IsParsed()) {
       DEBUGLOG("Stream with PID %i not parsed", (*i)->GetPID());
       bAllParsed = false;
